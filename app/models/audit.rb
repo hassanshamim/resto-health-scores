bb3847ca9b802cf254fb610429699bcbe9809f68
hassanshamim <hassan.has.you@gmail.com>
2012-11-28 17:59:30 -0600

    Updated scraper and restricted duplicate audits

--- app/models/audit.rb
+++ app/models/audit.rb
@@ -1,6 +1,8 @@
 class Audit < ActiveRecord::Base
-  attr_accessible :date, :score
+  attr_accessible :date, :score, :restaurant
   validates :date, :score, :restaurant, :presence => true
   validates :score, :numericality => true
+  validates :date, :uniqueness => { :scope => :restaurant_id,
+    :message => "A restaurant may not have more than one audit per day" }
   belongs_to :restaurant
 end
--- scraper/resto_audits.rb
+++ scraper/resto_audits.rb
@@ -4,18 +4,29 @@ require 'net/http'
 $remote_url = "http://health.nashville.gov/env/food/FoodScores/FoodScores.aspx?BegDate=08/1/2007&EndDate=11/12/2012"
 doc = Nokogiri::HTML(open($remote_url).read)
 nodes = doc.css('div#content > p')
+low_score_position = nodes.find_index{|node| node.inner_text.include?("Low Scores")}
+updates_position = nodes.find_index{|node| node.inner_text.include?("Updates")}
+
+#If there aren't any audits, all children.size will be < 4
+
+passing_nodes = nodes[ 0...low_score_position ].to_a
+failing_nodes = nodes[ ( low_score_position+1 )... updates_position ].to_a
+update_nodes = nodes[( (updates_position+1) ..-1)].to_a
+all_nodes = [passing_nodes, failing_nodes, update_nodes]
+
+all_nodes.each do |node_list|
+  node_list.reject!{ |node| node.children.size == 1 }  #Removes 'No Scores available' lines
+end
+
 #nodes.children[] 0, 2, 4, 6 contain relevant data
 #0 = Name, 2 = Address, 4 = Date, 6 = Score
-$passing_nodes = nodes.take_while{ |n| n.children.size > 3 } #failing audits have different format
-  #stops at the LOW SCORES node
-
 def create_audits(nodes)
   nodes.each do |node|
     children = node.children.map{ |s| s.text.strip }
     name = children[0]
     address = children[2]
     date = Date.strptime(children[4][/\d+\/\d+\/\d+/], '%m/%d/%Y')
-    score = children[6][/\d+$/]
+    score = children[6][/\d+$/].to_i
     resto = Restaurant.where( name: name, address: address ).first_or_create!
     Audit.create( date: date, score: score, restaurant: resto )
   end
@@ -32,3 +43,6 @@ def display_audits (nodes)
   end
   nil
 end
+
+create_audits( passing_nodes )
+create_audits( failing_nodes )
